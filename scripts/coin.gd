2e690447cd4d8c0bf40c97afa22e9e75fd730e1a
Zeta80 <axxel1700@gmail.com>
2025-02-02 21:13:41 +0100

    add coin sound animation player, finish the project

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
 @onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(body: Node2D) -> void:
 	print("+1 coin ehehe fabioooo")
 	game_manager.add_point()
-	queue_free()
+	animation_player.play("Pick_up")
